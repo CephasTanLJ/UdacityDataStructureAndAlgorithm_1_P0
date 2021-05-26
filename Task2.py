0a0bdfb67e0f44c34f5574df97aaddc8848bd124
Cephas Tan Li-Jie <cephastanlijie.tech@gmail.com>
2021-05-26 10:14:30 +0800

    Changed the algorithm for Task2 & update Analysis
    The previous method returned the longest call-entry.
    Current method return the longest call per user taking
        into account
            a call b -> 5s
            b call c -> 8s
            a call c -> 4s
        call per user: {"a": 9,
                        "b": 13,
                        "c": 12}

--- Task2.py
+++ Task2.py
@@ -3,6 +3,7 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -19,15 +20,28 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
-from datetime import datetime   # O(1)
-current_longest_time = 0        # O(1)
-longest_call = None             # O(1)
+call_time = dict()  # O(1)
 
-for entry in calls:                                                                 # O(n)
-    if float(entry[3]) > current_longest_time:                                  # +O(1)
-        longest_call = entry                                                    # +O(1)
-    #=> For Loop O() = O(n)
+for entry in calls:                                 # O(n)
+    phone_numbers = [entry[0], entry[1]]            # *O(1)
+    for phones in phone_numbers:                    # *O(2)
+        if phones not in call_time:                 # *O(1)
+            call_time[phones] = float(entry[3])     # +O(1)
+        else:
+            call_time[phones] += float(entry[3])
 
-print(f"{longest_call[0]} spent the longest time, {int(longest_call[3])} seconds, on the phone during September 2016.") # O(1)
+longest_call_num = max(call_time, key=call_time.get)    # O(n) - assuming it checks every element
 
-#RunTime analysis ~ O(n + 4) => O(n)
+#Test
+# def test():
+#     assert call_time[
+#                '78130 00821'] == 26368.0, f"Not correct calculations {call_time['78130 00821']} instead of 26368.0"
+#
+#     sorted_dictionary = dict(sorted(call_time.items(), key=lambda item: item[1]))
+#     assert longest_call_num == sorted_dictionary.popitem()[0], f"longest_call_num {longest_call_num} is not correct"
+#
+# test()
+
+print(f"{longest_call_num} spent the longest time, {call_time[longest_call_num]} seconds, on the phone during September 2016.")  # O(1)
+
+# RunTime analysis ~ O(n)
